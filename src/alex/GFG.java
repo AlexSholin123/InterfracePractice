ff9159b9353247c760f2b32d84307ba50886b6cc
AlexSholin <82291878+AlexSholin123@users.noreply.github.com>
2022-11-24 21:08:18 +0200

    add xxx zzz

--- src/alex/GFG.java
+++ src/alex/GFG.java
@@ -20,7 +20,11 @@ class SDE2 implements salary {
     {
         this.salary = salary;
     }
-    void printSalary() { System.out.println(this.salary); }
+    void printSalary() {
+        System.out.println("xxx");
+        System.out.println(this.salary);
+        System.out.println("zzz");
+    }
 }
 
 public class GFG {
